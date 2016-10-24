4212ce1ffbb01d0803e1d381aeb4f83fb6bdc7a7
Marc Nieper-Wißkirchen <marc@nieper-wisskirchen.de>
2016-10-24 22:42:24 +0200

    Current version @hodge

--- init.el
+++ init.el
@@ -2,6 +2,33 @@
 
 ;; Copyright (C) 2015-2016 Marc Nieper-Wißkirchen
 
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+;; Asm Mode
+(setq asm-comment-char ?#)
+
+;; Paredit
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
+
+;; Outline Mode
+(add-hook 'scheme-mode-hook 'outline-minor-mode)
+
+(require 'cc-mode)
+(require 'font-lock)
+
+(require 'ox-latex)
+(setq org-latex-listings t)
+(setq org-latex-listings-options
+      '(("basicstyle" "\\ttfamily")
+	("columns" "flexible")
+	("literate" "{<<}{<<}1 {>>}{>>}1")))
+(add-to-list 'org-latex-packages-alist '("" "listings"))
+(add-to-list 'org-latex-packages-alist '("" "color"))
+(require 'ox-beamer)
+(setq org-src-preserve-indentation t)
+
 (global-font-lock-mode 1)
 
 (setq show-paren-delay 0
@@ -41,7 +68,48 @@
 			  ("(\\(for*\\)\\>" 1 font-lock-keyword-face)
 			  ("(\\(case-lambda\\)\\>" 1 font-lock-keyword-face)
 			  ("(\\(include\\)\\>" 1 font-lock-keyword-face)))
-			  
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((eval setq org-src-preserve-indentation t)
+     (eval font-lock-add-keywords
+	   (quote scheme-mode)
+	   (quote
+	    (("(\\(define-syntactic-environment\\)\\>" 1 font-lock-keyword-face)
+	     ("(\\(define-auxiliary-syntax\\)\\>" 1 font-lock-keyword-face)
+	     ("(\\(define-transformer\\)\\>" 1 font-lock-keyword-face))))
+     (eval font-lock-add-keywords
+	   (quote scheme-mode)
+	   (quote
+	    (("(\\(define-syntactic-environment\\)\\>" 1 font-lock-keyword-face)
+	     ("(\\(define-transformer\\)\\>" 1 font-lock-keyword-face))))
+     (eval font-lock-add-keywords
+	   (quote scheme-mode)
+	   (quote
+	    (("(\\(define-syntactic-environment\\)\\>" 1 font-lock-keyword-face)))
+	   ("(\\(define-transformer\\)\\>" 1 font-lock-keyword-face))
+     (eval font-lock-add-keywords
+	   (quote scheme-mode)
+	   (quote
+	    (("(\\(define-syntactic-environment\\)\\>" 1 font-lock-keyword-face))))
+     (eval put
+	   (quote define-syntactic-environment)
+	   (quote defun))
+     (eval font-lock-add-keywords
+	   (quote scheme-mode)
+	   (quote
+	    (("(\\(receive\\)\\>" 1 font-lock-keyword-face))))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; Local Variables:
 ;; mode: emacs-lisp
 ;; End:
